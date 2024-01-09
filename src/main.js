0501836aecf0dfc3f2e211aaaa78629f52026505
Mariia69 <141348185+Mariia69@users.noreply.github.com>
2024-01-09 19:08:34 +0200

    update main.js

--- src/main.js
+++ src/main.js
@@ -1,28 +1,118 @@
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
+const BASE_URL = 'https://pixabay.com/api';
+const galleryContainer = document.querySelector('.gallery');
+const loader = document.querySelector('.loader');
 
+document.addEventListener('DOMContentLoaded', function () {
+  document.querySelector('.form').addEventListener('submit', function (event) {
+    event.preventDefault();
 
-const BASE_URL = 'https://pixabay.com/api/';
-const API_KEY = '41701983-23ca5d5908e2c78927e8095f2';
+    loader.style.display = 'block';
 
-const getBaseUrl = () => {
-  const url = new URL(BASE_URL);
-  url.searchParams.append("key", API_KEY)
+    const searchQuery = document.querySelector('.input').value;
+    searchImages(searchQuery);
+  });
+});
 
-  return url;
+function searchImages(query) {
+  const searchParams = {
+    key: '41530173-f95b78bdec41263a85620f647',
+    q: query,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: 'true',
+  };
+
+  fetch(`${BASE_URL}/?${new URLSearchParams(searchParams)}`)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
+    })
+    .then(data => {
+      loader.style.display = 'none';
+      loader.classList.remove('loader--active');
+
+      displayImages(data.hits);
+    })
+    .catch(() => {
+      loader.style.display = 'none';
+      loader.classList.remove('loader--active');
+
+      showErrorToast();
+    });
+
+  function displayImages(images) {
+    galleryContainer.innerHTML = '';
+
+    if (images.length > 0) {
+      const imageCards = images.map(image => {
+        const card = document.createElement('div');
+        card.classList.add('card');
+
+        const largeImageLink = document.createElement('a');
+        largeImageLink.href = image.largeImageURL;
+        largeImageLink.dataset.lightbox = 'gallery';
+        largeImageLink.setAttribute('data-title', image.tags);
+
+        const img = document.createElement('img');
+        img.src = image.webformatURL;
+        img.alt = image.tags;
+
+        largeImageLink.appendChild(img);
+
+        card.appendChild(largeImageLink);
+
+        const details = document.createElement('div');
+        details.classList.add('details');
+
+        const likes = document.createElement('p');
+        likes.textContent = `Likes: ${image.likes}`;
+        details.appendChild(likes);
+
+        const views = document.createElement('p');
+        views.textContent = `Views: ${image.views}`;
+        details.appendChild(views);
+
+        const comments = document.createElement('p');
+        comments.textContent = `Comments: ${image.comments}`;
+        details.appendChild(comments);
+
+        const downloads = document.createElement('p');
+        downloads.textContent = `Downloads: ${image.downloads}`;
+        details.appendChild(downloads);
+
+        card.appendChild(details);
+
+        return card;
+      });
+
+      galleryContainer.append(...imageCards);
+
+      const lightbox = new SimpleLightbox('.gallery a', {});
+      lightbox.refresh();
+    } else {
+      showNoImagesFoundToast();
+    }
+  }
 }
 
-const url = getBaseUrl();
-url.searchParams.append("q", "wolf");
+function showErrorToast() {
+  iziToast.error({
+    title: 'Error',
+    message: 'An error occurred. Please try again later.',
+  });
+}
 
-fetch(url)
-  .then(res => res.json())
-  .then(images => {
-    document.body.insertAdjacentHTML("beforeend", `
-    <img src="${images.hits[0].largeImageURL}"/>
-    `)
-  })
-  
+function showNoImagesFoundToast() {
+  iziToast.info({
+    title: 'No Images Found',
+    message:
+      'Sorry, there are no images matching your search query. Please try again!',
+  });
+}
